bdaf7e7b4674ac74a597407febcf240e0a66d609
Gopikrishhna Chokkalamani <gopikrishhna@gmail.com>
2023-03-15 21:29:06 -0500

    commit-message

---
+++ 6-terraform-eks-cluster/vpc.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "3.19.0"
+  name                 = "my-terraform-eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
