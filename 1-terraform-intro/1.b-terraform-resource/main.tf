2e522444b8cd95e91f1f2833bcc76c90172d67eb
Gopikrishhna Chokkalamani <gopikrishhna@gmail.com>
2023-03-24 14:11:07 -0500

    learning

--- 1-terraform-intro/1.b-terraform-resource/main.tf
+++ 1-terraform-intro/1.b-terraform-resource/main.tf
@@ -23,6 +23,12 @@ variable "region" {
   default = "us-east-1"
 }
 
+// this will error out, variables cannot be used as reference in another variable
+//variable "hello-world" {
+//  default = var.region
+//  description = "hello world"
+//}
+
 resource "aws_instance" "dev" {
   ami = "ami-006dcf34c09e50022"
   instance_type = "t3.micro"
